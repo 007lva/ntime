a2df90ca38b09020d4d75d6986753f6a224f14ba
Martín Zamuner <martin@rankia.com>
2018-02-07 14:20:20 +0100

    Show last clocking

--- src/nettime.cr
+++ src/nettime.cr
@@ -13,11 +13,16 @@ today = Time::Format.new("%F").format(Time.now).to_s # Date in ISO8601
 request1 = Crest.post("http://192.168.1.200:8091/api/login", payload: "{\"username\":\"#{ENV["user"]}\",\"pwd\":\"#{ENV["pwd"]}\"}")
 
 request2 = Crest.get("http://192.168.1.200:8091/api/results", params: {"from" => today, "to" => today}, cookies: request1.cookies)
+request3 = Crest.get("http://192.168.1.200:8091/api/clockings", params: {"date" => today}, cookies: request1.cookies)
 
 sleep 1
 
 request4 = Crest.get("http://192.168.1.200:8091/api/async/response", params: {"taskid" => Task.from_json(request2.body).taskId}, cookies: request2.cookies)
+request5 = Crest.get("http://192.168.1.200:8091/api/async/response", params: {"taskid" => Task.from_json(request3.body).taskId}, cookies: request3.cookies)
 
+last_clocking = JSON.parse(request5.body)["clockings"]
+                    .map { |item| item["date"].to_s }
+                    .last
 results = JSON.parse(request4.body)["results"][0]["minutesTypes"]
               .select { |item| item["name"] == "Aritmetico" }
               .map { |item| item["results"] }
