4e78e6b6a1f0e8e5afe90d7e39337e101f800dc7
luigi <007.lva@gmail.com>
2018-11-03 12:43:45 -0500

    Support Crystal 0.27

--- src/ntime.cr
+++ src/ntime.cr
@@ -20,8 +20,9 @@ sleep 0.07
 request4 = Crest.get("http://192.168.1.200:8091/api/async/response", params: {"taskid" => Task.from_json(request2.body).taskId}, cookies: request2.cookies)
 request5 = Crest.get("http://192.168.1.200:8091/api/async/response", params: {"taskid" => Task.from_json(request3.body).taskId}, cookies: request3.cookies)
 
-last_clocking = JSON.parse(request5.body)["clockings"].to_a.last
+last_clocking = JSON.parse(request5.body)["clockings"].as_a.last
 results = JSON.parse(request4.body)["results"][0]["minutesTypes"]
+              .as_a
               .select { |item| item["name"] == "Aritmetico" }
               .map { |item| item["results"] }
               .first
@@ -30,4 +31,4 @@ puts "Saldo diario acumulado: #{results[0]["values"][1]["value"].as_i} min"
 puts "Saldo mensual acumulado: #{results[1]["values"][1]["value"].as_i} min"
 puts "Saldo anual acumulado: #{results[2]["values"][1]["value"].as_i} min"
 puts "Días de vacaciones utilizados: #{results[3]["values"][1]["value"].as_i}"
-puts "Último movimiento: #{Time.parse(last_clocking["date"].to_s, "%FT%T")} (#{last_clocking["status"]["desc"]})"
+puts "Último movimiento: #{Time.parse_local(last_clocking["date"].to_s, "%FT%T")} (#{last_clocking["status"]["desc"]})"
